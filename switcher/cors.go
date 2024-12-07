6fde0d6eb073b163a88b6032949a3ddcb7c4a378
protolambda <proto@protolambda.com>
2024-12-07 21:42:22 +0100

    switcher: api CORS handling

---
+++ switcher/cors.go
@@ -0,0 +1,13 @@
+package switcher
+
+import "net/http"
+
+func addCorsHeader(res http.ResponseWriter) {
+	headers := res.Header()
+	headers.Add("Access-Control-Allow-Origin", "*")
+	headers.Add("Vary", "Origin")
+	headers.Add("Vary", "Access-Control-Request-Method")
+	headers.Add("Vary", "Access-Control-Request-Headers")
+	headers.Add("Access-Control-Allow-Headers", "*")
+	headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+}
